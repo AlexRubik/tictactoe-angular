c7481cade093444e01e37eed5a8da8b273867ec6
dawg <dewittat@g.cofc.edu>
2022-07-13 17:44:11 -0400

    display grid for html body

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -9,8 +9,8 @@ import { Component, Input } from '@angular/core';
 
 
     <button class="square" *ngIf="value == null">{{value}}</button>
-    <button class="Xsquare" *ngIf="value == 'X'">{{value}}</button>
-    <button class="Osquare" *ngIf="value == 'O'">{{value}}</button>
+    <!-- <button class="Xsquare" *ngIf="value == 'X'">{{value}}</button>
+    <button class="Osquare" *ngIf="value == 'O'">{{value}}</button> -->
   `,
  styleUrls:["./sq.scss"]
 
